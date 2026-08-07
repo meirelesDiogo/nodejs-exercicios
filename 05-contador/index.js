fb0ee967f29a249800319b61329c53745c44cd1d
Diogo Alexandre <didialexandre1017@gmail.com>
2026-08-06 21:27:49 -0300

    CONTADOR

--- 05-contador/index.js
+++ 05-contador/index.js
@@ -20,12 +20,10 @@ rl.question("➜ ", function (ent) {
   console.log("          NÚMEROS DE 1 A 100");
   console.log("=========================================\n");
 
-  let i = 1;
-
-  while (i <= 100) {
+  for (let i = 1; i <= 100; i++) {
     console.log(i);
-    i++;
   }
+
   console.log("\n=========================================");
   console.log("➡️  Aperte ENTER para mostrar apenas os números pares.");
   console.log("=========================================\n");
@@ -37,19 +35,30 @@ rl.question("➜ ", function (ent) {
     console.log("         NÚMEROS PARES (1 a 100)");
     console.log("=========================================\n");
 
-    let i = 1;
-
-    while (i <= 100) {
-      if (i % 2 == 0) {
-        console.log(i);
-      }
-      i++;
+    for (let i = 1; i <= 100; i++) {
+      if (i % 2 === 0) console.log(i);
     }
 
     console.log("\n=========================================");
-    console.log("           ✔️ FIM DO PROGRAMA");
-    console.log("=========================================");
+    console.log("➡️  Aperte ENTER para mostrar apenas os números ímpares.");
+    console.log("=========================================\n");
 
-    rl.close();
+    rl.question("➜ ", function (enter) {
+      console.clear();
+
+      console.log("=========================================");
+      console.log("        NÚMEROS ÍMPARES (1 a 100)");
+      console.log("=========================================\n");
+
+      for (let i = 1; i <= 100; i++) {
+        if (i % 2 !== 0) console.log(i);
+      }
+
+      console.log("\n=========================================");
+      console.log("           ✔️ FIM DO PROGRAMA");
+      console.log("=========================================");
+
+      rl.close();
+    });
   });
 });
