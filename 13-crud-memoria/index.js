9f56edec3a8a956fc77f47ff12cae1d65808f67e
Diogo Alexandre <didialexandre1017@gmail.com>
2026-08-09 21:48:49 -0300

    inicio do crud

---
+++ 13-crud-memoria/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+
+const port = 3000;
+const fs = require("fs");
+const dados = [
+  {
+    id: 1,
+    nome: "Diogo",
+    email: "diogo@gmail.com",
+    cidade: "Belo Horizonte",
+  },
+  {
+    id: 2,
+    nome: "Diego",
+    email: "diego@gmail.com",
+    cidade: "Sao Paulo",
+  },
+  {
+    id: 3,
+    nome: "Joao Silva",
+    email: "joao@gmail.com",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  if (url === "/" && method === "GET") {
+    fs.readFile("index.html", (erro, arquivo) => {
+      //vai le o html e mostrar na pagina inicial
+      if (erro) {
+        console.log("Erro ao Ler Index:", erro);
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+      res.end(arquivo);
+    });
+  } else if (url === "/usuarios") {
+    let corpo = "";
+    req.on("data", (pedaco) => {
+      corpo += pedaco;
+    });
+    req.on("end", () => {
+      const object = JSON.parse(corpo);
+      dados.push(object);
+      console.log(dados);
+    });
+  }
+});
+
+server.listen(port, "localhost", () => {
+  console.log("Server Is Running in port", port);
+});
