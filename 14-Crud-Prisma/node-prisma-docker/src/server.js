2989a49f441c28cbb7777e6522347d9cc2aaf591
Diogo Alexandre <didialexandre1017@gmail.com>
2026-08-18 10:42:38 -0300

    add Method POST

--- 14-Crud-Prisma/node-prisma-docker/src/server.js
+++ 14-Crud-Prisma/node-prisma-docker/src/server.js
@@ -26,16 +26,54 @@ const server = http.createServer(async (req, res) => {
     });
 
     if (user) {
-      console.log("Deu bom pae");
-      res.end;
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
     } else {
       res.setHeader("Content-Type", "text/html;charset=utf-8");
 
       res.write(
-        "<script>alert('Não foi encontrado Usuario com esse ID')</script>",
+        "<script>alert('Não foi encontrado Usuario com esse ID');window.location.href='/pessoas';</script>",
       );
+
       res.end();
     }
+  } else if (url === "/cadastro" && method === "GET") {
+    fs.readFile("src/criacao.html", "utf-8", (erro, conteudo) => {
+      if (erro) {
+        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end("Erro interno: Não foi possível carregar a página.");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(conteudo);
+    });
+  } else if (url === "/pessoas" && method === "POST") {
+    let corpo = "";
+
+    req.on("data", (pedaco) => {
+      corpo += pedaco;
+    });
+    req.on("end", async () => {
+      const usuario = JSON.parse(corpo); // transforma em objeto
+
+      const NovaPessoa = await prisma.pessoa.create({ // Add um novo usuario ao bd
+        data: {
+          nome: usuario.nome,
+          cidade: usuario.cidade,
+          email: usuario.email,
+        },
+      });
+      if (NovaPessoa) {
+        console.log("Novo Usuario Adicionado");
+      } else {
+        console.log("Usuario Nao foi Adicionado AO BD");
+      }
+
+      // 2. RESPONDE O NAVEGADOR falando q deu certo
+      res.writeHead(201, { "Content-Type": "application/json; charset=utf-8" });
+      res.end();
+    });
   }
 });
 
