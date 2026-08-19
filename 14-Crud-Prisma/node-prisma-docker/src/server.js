06d897695f1f327dbdaf139e5b27aa93db512e75
Diogo Alexandre <didialexandre1017@gmail.com>
2026-08-18 22:05:59 -0300

    tratamento de erros e busca pra novas url's

--- 14-Crud-Prisma/node-prisma-docker/src/server.js
+++ 14-Crud-Prisma/node-prisma-docker/src/server.js
@@ -9,10 +9,15 @@ const server = http.createServer(async (req, res) => {
 
   if (url === "/pessoas" && method === "GET") {
     //Lista todos os usuarios
-    res.setHeader("Content-Type", "application/json");
-    const users = await prisma.pessoa.findMany(); // Pega tudo da tabela Pessoa
 
-    res.end(JSON.stringify(users));
+    const users = await prisma.pessoa.findMany(); // Pega tudo da tabela Pessoa
+    if (users) {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(users));
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ erro: "Usuario Nao foi cadastrado" }));
+    }
   } else if (url.startsWith("/pessoas/") && method === "GET") {
     //Pega e mostra a pessoa pelo id
 
@@ -57,7 +62,8 @@ const server = http.createServer(async (req, res) => {
     req.on("end", async () => {
       const usuario = JSON.parse(corpo); // transforma em objeto
 
-      const NovaPessoa = await prisma.pessoa.create({ // Add um novo usuario ao bd
+      const NovaPessoa = await prisma.pessoa.create({
+        // Add um novo usuario ao bd
         data: {
           nome: usuario.nome,
           cidade: usuario.cidade,
@@ -65,15 +71,81 @@ const server = http.createServer(async (req, res) => {
         },
       });
       if (NovaPessoa) {
-        console.log("Novo Usuario Adicionado");
+        // 2. RESPONDE O NAVEGADOR falando q deu certo
+        res.writeHead(201, {
+          "Content-Type": "application/json; charset=utf-8",
+        });
+        res.end();
       } else {
         console.log("Usuario Nao foi Adicionado AO BD");
       }
-
-      // 2. RESPONDE O NAVEGADOR falando q deu certo
-      res.writeHead(201, { "Content-Type": "application/json; charset=utf-8" });
-      res.end();
     });
+  } else if (url.startsWith("/atualizar/") && method === "GET") {
+    const partes = url.split("/");
+    const id = parseInt(partes[2]);
+
+    fs.readFile("src/atualiza.html", "utf-8", async (erro, arquivo) => {
+      if (erro) {
+        res.writeHead(400, {
+          "Content-Type": "application/json;charset=utf-8",
+        });
+        res.end(
+          JSON.stringify({ erro: "Nao foi possivel Carregar o Arquivo" }),
+        );
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+
+      const UsuarioEncontrado = await prisma.pessoa.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      const HtmlModificado = arquivo
+        .replace("{{id}}", UsuarioEncontrado.id)
+        .replace("{{nome}}", UsuarioEncontrado.nome)
+        .replace("{{cidade}}", UsuarioEncontrado.cidade)
+        .replace("{{email}}", UsuarioEncontrado.email);
+
+      res.end(HtmlModificado);
+    });
+  } else if (url.startsWith("/pessoas") && method === "PUT") {
+    let corpo = "";
+    req.on("data", (pedaco) => {
+      corpo += pedaco;
+    });
+    req.on("end", async () => {
+      const usuario = JSON.parse(corpo);
+      const id = usuario.id;
+
+      const resposta = await prisma.pessoa.update({
+        // atualiza os cadastros no BD
+        where: { id: Number(id) },
+        data: {
+          nome: usuario.nome,
+          cidade: usuario.cidade,
+          email: usuario.email,
+        },
+      });
+      if (resposta) {
+        res.writeHead(201, {
+          "Content-Type": "application/json;charset=utf-8",
+        });
+        res.end();
+      }
+    });
+  } else if (url.startsWith("/deletar/" && method === "GET")) {
+    const partes = url.split("/");
+    const id = parseInt(partes[2]);
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+    res.write("Você será direcionado para a pagina de DELETE");
+    
+    res.end();
+  } else {
+    // 3.  Resposta padrão para qualquer outra rota
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Nao encontrado");
   }
 });
 
